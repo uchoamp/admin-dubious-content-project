13030fea419c52c9c05bcbb62a0261663a99b204
Marcos Pacheco <azurique10111@gmail.com>
2022-03-07 01:27:31 -0300

    Fix: keys of database

--- src/database.js
+++ src/database.js
@@ -1,19 +1,22 @@
 const mongoose = require("mongoose");
 
-const {DATABASE_HOST, DATABASE, DATABASE_USER, DATABASE_PASSWORD} = process.env;
+const { DATABASE_HOST, DATABASE_USER, DATABASE, DATABASE_PORT, DATABASE_PASSWORD} = process.env;
 
-const mongodb_URI = `mongodb+srv://${DATABASE_USER}:${DATABASE_PASSWORD}@${DATABASE_HOST}/${DATABASE}?retryWrites=true&w=majority`;
-
+const mongodb_URI =
+  (!DATABASE_PORT) ? `mongodb+srv://${DATABASE_USER}:${DATABASE_PASSWORD}@${DATABASE_HOST}/${DATABASE}?retryWrites=true&w=majority` :
+   (DATABASE_USER && DATABASE_PASSWORD) ?
+  `mongodb://${DATABASE_USER}:${DATABASE_PASSWORD}@${DATABASE_HOST}:${DATABASE_PORT}/${DATABASE}` :
+  `mongodb://${DATABASE_HOST}:${DATABASE_PORT}/${DATABASE}`;
 
 mongoose.connect(mongodb_URI, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-})
-.then((db)=>{
-    console.log(DATABASE, "is connected.");
-}).catch((err)=>{
-    console.error(err);
-});
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+}).
+then(db => {console.log(DATABASE, 'is conected.')}).
+catch(err => {console.log(err)});
 
+
+
+module.exports = mongoose;
